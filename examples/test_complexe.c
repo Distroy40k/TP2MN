42ec86f6bf515a9232a294efde00333c9e4d7dea
Yael Para <yael.para@etu.univ-grenoble-alpes.fr>
2020-03-04 14:39:18 +0100

    Test des fonctions d'addition et de multiplication de complexes

--- examples/test_complexe.c
+++ examples/test_complexe.c
@@ -10,6 +10,7 @@
 
 int main (int argc, char **argv)
 {
+
  complexe_float_t c1= {1.0, 2.0} ;
  complexe_float_t c2= {3.0, 6.0} ;
  complexe_float_t c3 ;
@@ -20,6 +21,12 @@ int main (int argc, char **argv)
 
  unsigned long long int start, end ;
  int i ;
+
+ // Partie addition
+
+ printf("\n");
+ printf("Tests des fonctions d'addition de complexes\n");
+ printf("\n");
 
  init_flop () ;
  
@@ -46,5 +53,41 @@ int main (int argc, char **argv)
   printf ("apres boucle cd3.real %f cd3.imaginary %f %lld cycles \n", cd3.real, cd3.imaginary, end-start) ;
 
   calcul_flop ("calcul complexe ", NB_FOIS*4, end-start) ;
+
+
+  // Partie multiplication
+
+  printf("\n");
+  printf("Tests des fonctions de multiplication de complexes\n");
+  printf("\n");
+
+  init_flop () ;
+ 
+ c3 = mult_complexe_float (c1, c2) ;
+
+ printf ("c3.r %f c3.i %f\n", c3.real, c3.imaginary) ;
+
+ cd1 = (complexe_double_t) {10.0, 7.0} ;
+ cd2 = (complexe_double_t) {25.0, 32.0} ;
+
+ cd3 = mult_complexe_double (cd1, cd2) ;
+
+ printf ("cd3.r %f cd3.i %f\n", cd3.real, cd3.imaginary) ;
+
+ start =_rdtsc () ;
+ 
+ for (i = 0 ; i < NB_FOIS; i++)
+   {
+     cd3 = mult_complexe_double (cd1, cd2) ;
+   }
+
+ end = _rdtsc () ;
+
+  printf ("apres boucle cd3.real %f cd3.imaginary %f %lld cycles \n", cd3.real, cd3.imaginary, end-start) ;
+
+  calcul_flop ("calcul complexe ", NB_FOIS*4, end-start) ;
+
+
+
   exit (0) ;
 }
