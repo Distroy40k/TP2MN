2f715b9c489a64e6052d91e268a881c115bb7c85
Alexis Lanquetin <alexis.lanquetin@etu.univ-grenoble-alpes.fr>
2020-03-17 08:58:33 +0100

    Modification de flop.c

--- test/flop.h
+++ test/flop.h
@@ -2,3 +2,5 @@
 void init_flop () ;
 
 void calcul_flop (char *message, int nb_operations_flottantes, unsigned long long int cycles) ;
+
+void calcul_octet (char *message, int nb_operations_octet, unsigned long long int cycles);
