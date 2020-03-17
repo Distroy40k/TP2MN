62e18d8b74a6bb9d89267bb0c88a7b81f31ff331
Alexis Lanquetin <alexis.lanquetin@etu.univ-grenoble-alpes.fr>
2020-03-17 09:20:40 +0100

    dot.c V1, a completer tout les fonctions presques

--- src/dot.c
+++ src/dot.c
@@ -2,13 +2,13 @@
 #include <stdio.h>
 
 /*
-float mncblas_sdot(const int N, const float *X, const int incX, 
+float mncblas_sdot(const int N, const float *X, const int incX,
                  const float *Y, const int incY)
 {
   register unsigned int i = 0 ;
   register unsigned int j = 0 ;
   register float dot = 0.0 ;
-  
+
   for (; ((i < N) && (j < N)) ; i += incX, j+=incY)
     {
       dot = dot + X [i] * Y [j] ;
@@ -18,14 +18,14 @@ float mncblas_sdot(const int N, const float *X, const int incX,
 }
 */
 
-float mncblas_sdot(const int N, const float *X, const int incX, 
+float mncblas_sdot(const int N, const float *X, const int incX,
                  const float *Y, const int incY)
 {
   register unsigned int i = 0 ;
   register unsigned int j = 0 ;
   float dot = 0.0 ;
 
-  
+
   for (i = 0 ; i < N ; i += incX)
     {
       dot += X [i] * Y [j] ;
@@ -35,13 +35,22 @@ float mncblas_sdot(const int N, const float *X, const int incX,
   return dot ;
 }
 
-double mncblas_ddot(const int N, const double *X, const int incX, 
+double mncblas_ddot(const int N, const double *X, const int incX,
                  const double *Y, const int incY)
 {
-  /*
-     a completer
-  */
-  
+  register unsigned int i = 0 ;
+  register unsigned int j = 0 ;
+  double dot = 0.0 ;
+
+
+  for (i = 0 ; i < N ; i += incX)
+    {
+      dot += X [i] * Y [j] ;
+      j+=incY ;
+    }
+
+  return dot ;
+
   return 0.0;
 }
 
@@ -49,7 +58,7 @@ void   mncblas_cdotu_sub(const int N, const void *X, const int incX,
                        const void *Y, const int incY, void *dotu)
 {
   /* a completer */
-  
+
   return ;
 }
 
@@ -57,7 +66,7 @@ void   mncblas_cdotc_sub(const int N, const void *X, const int incX,
                        const void *Y, const int incY, void *dotc)
 {
   /* a completer */
-  
+
   return ;
 }
 
@@ -65,18 +74,14 @@ void   mncblas_zdotu_sub(const int N, const void *X, const int incX,
                        const void *Y, const int incY, void *dotu)
 {
   /* a completer */
-  
+
   return ;
 }
-  
+
 void   mncblas_zdotc_sub(const int N, const void *X, const int incX,
                        const void *Y, const int incY, void *dotc)
 {
   /* a completer */
-  
+
   return ;
 }
-
-
-
-
