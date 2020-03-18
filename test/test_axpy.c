62682aab34c6eb9186b90df56272cdb5739d1686
Yaël <ya38able@gmail.com>
2020-03-18 14:37:30 +0100

    Fichier de test des fonctions axpy

---
+++ test/test_axpy.c
@@ -0,0 +1,230 @@
+#include <stdio.h>
+#include <x86intrin.h>
+
+#include "mnblas.h"
+
+#include "flop.h"
+
+#define NB_FOIS 4000
+#define VECSIZE 4
+
+void fvector_init(float *V)
+{
+  register unsigned int i;
+
+  for (i = 0; i < VECSIZE; i++)
+    V[i] = i;
+
+  return;
+}
+
+void dvector_init(double *V)
+{
+  register unsigned int i;
+
+  for (i = 0; i < VECSIZE; i++)
+    V[i] = i;
+
+  return;
+}
+
+void fvector_print(float *V)
+{
+  register unsigned int i;
+
+  for (i = 0; i < VECSIZE; i++)
+    printf("%f ", V[i]);
+  printf("\n");
+
+  return;
+}
+
+void dvector_print(double *V)
+{
+  register unsigned int i;
+
+  for (i = 0; i < VECSIZE; i++)
+    printf("%f ", V[i]);
+  printf("\n");
+
+  return;
+}
+
+int max(int a, int b)
+{
+  if (a > b)
+  {
+    return a;
+  }
+  return b;
+}
+
+int main(int argc, char **argv)
+{
+
+  float *fX = (float *)malloc(sizeof(float) * VECSIZE);
+  float *fY = (float *)malloc(sizeof(float) * VECSIZE);
+  double *dX = (double *)malloc(sizeof(double) * VECSIZE);
+  double *dY = (double *)malloc(sizeof(double) * VECSIZE);
+  double *dalpha = (double *)malloc(sizeof(double) * 1);
+  float *falpha = (float *)malloc(sizeof(float) * 1);
+
+  falpha[0] = 2.0;
+  dalpha[0] = 2.0;
+  float alpha = 2.0;
+
+  int incX = 1;
+  int incY = 1;
+  int nb_boucles;
+
+  fvector_init(fX);
+  fvector_init(fY);
+  dvector_init(dX);
+  dvector_init(dY);
+
+  unsigned long long int start, end;
+  int i;
+
+  init_flop();
+
+  printf("\n########### TEST #############\nFonction mnblas_saxpy\n\n");
+
+  printf("%f * X + Y\n", alpha);
+  printf("X :\n");
+  fvector_print(fX);
+  printf("Y :\n");
+  fvector_print(fY);
+  mnblas_saxpy(VECSIZE, alpha, fX, incX, fY, incY);
+  printf("\n");
+  printf("Resultat :\n");
+  fvector_print(fY);
+
+  fvector_init(fY);
+  mnblas_saxpy(VECSIZE, alpha, fX, incX + 1, fY, incY);
+  printf("Resultat avec incX = 2 :\n");
+  fvector_print(fY);
+
+  fvector_init(fY);
+
+  start = _rdtsc();
+  for (i = 0; i < NB_FOIS; i++)
+  {
+    mnblas_saxpy(VECSIZE, alpha, fX, incX, fY, incY);
+  }
+  end = _rdtsc();
+
+  printf("mnblas_saxpy %d : nombre de cycles: %Ld \n", i, end - start);
+
+  nb_boucles = VECSIZE / max(incX, incY);
+  calcul_flop("mnblas_saxpy ", nb_boucles * 4, end - start);
+  printf("\n");
+
+  printf("\n########### TEST #############\nFonction mnblas_daxpy\n\n");
+
+  printf("%lf * X + Y\n", alpha);
+  printf("X :\n");
+  dvector_print(dX);
+  printf("Y :\n");
+  dvector_print(dY);
+  mnblas_daxpy(VECSIZE, alpha, dX, incX, dY, incY);
+  printf("\n");
+  printf("Resultat :\n");
+  dvector_print(dY);
+
+  dvector_init(dY);
+  mnblas_daxpy(VECSIZE, alpha, dX, incX + 1, dY, incY);
+  printf("Resultat avec incX = 2 :\n");
+  dvector_print(dY);
+
+  dvector_init(dY);
+
+  start = _rdtsc();
+  for (i = 0; i < NB_FOIS; i++)
+  {
+    mnblas_daxpy(VECSIZE, alpha, dX, incX, dY, incY);
+  }
+  end = _rdtsc();
+
+  printf("mnblas_daxpy %d : nombre de cycles: %Ld \n", i, end - start);
+
+  nb_boucles = VECSIZE / max(incX, incY);
+  calcul_flop("mnblas_daxpy ", nb_boucles * 4, end - start);
+  printf("\n");
+
+  printf("\n########### TEST #############\nFonction mnblas_caxpy\n\n");
+
+  fvector_init(fY);
+
+  printf("%f * X + Y\n", falpha[0]);
+  printf("X :\n");
+  fvector_print(fX);
+  printf("Y :\n");
+  fvector_print(fY);
+  mnblas_caxpy(VECSIZE, falpha, fX, incX, fY, incY);
+  printf("\n");
+  printf("Resultat :\n");
+  fvector_print(fY);
+
+  fvector_init(fY);
+  mnblas_caxpy(VECSIZE, falpha, fX, incX + 1, fY, incY);
+  printf("Resultat avec incX = 2 :\n");
+  fvector_print(fY);
+
+  fvector_init(fY);
+
+  start = _rdtsc();
+  for (i = 0; i < NB_FOIS; i++)
+  {
+    mnblas_caxpy(VECSIZE, falpha, fX, incX, fY, incY);
+  }
+  end = _rdtsc();
+
+  printf("mnblas_caxpy %d : nombre de cycles: %Ld \n", i, end - start);
+
+  nb_boucles = VECSIZE / max(incX, incY);
+  calcul_flop("mnblas_caxpy ", nb_boucles * 10, end - start);
+  printf("\n");
+
+  printf("\n########### TEST #############\nFonction mnblas_zaxpy\n\n");
+
+  dvector_init(dY);
+
+  printf("%lf * X + Y\n", dalpha[0]);
+  printf("X :\n");
+  dvector_print(dX);
+  printf("Y :\n");
+  dvector_print(dY);
+  mnblas_zaxpy(VECSIZE, dalpha, dX, incX, dY, incY);
+  printf("\n");
+  printf("Resultat :\n");
+  dvector_print(dY);
+
+  dvector_init(dY);
+  mnblas_zaxpy(VECSIZE, dalpha, dX, incX + 1, dY, incY);
+  printf("Resultat avec incX = 2 :\n");
+  dvector_print(dY);
+
+  dvector_init(dY);
+
+  start = _rdtsc();
+  for (i = 0; i < NB_FOIS; i++)
+  {
+    mnblas_zaxpy(VECSIZE, dalpha, dX, incX, dY, incY);
+  }
+  end = _rdtsc();
+
+  printf("mnblas_zaxpy %d : nombre de cycles: %Ld \n", i, end - start);
+
+  nb_boucles = VECSIZE / max(incX, incY);
+  calcul_flop("mnblas_zaxpy ", nb_boucles * 10, end - start);
+  printf("\n");
+
+  free(fX);
+  free(fY);
+  free(dX);
+  free(dY);
+  free(falpha);
+  free(dalpha);
+
+  exit(0);
+}
