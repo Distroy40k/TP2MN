a555b28b5659adfb56312141fda5c159400b4828
Alexis Lanquetin <alexis.lanquetin@etu.univ-grenoble-alpes.fr>
2020-03-18 13:51:45 +0100

    Correction utils.h, def des inline

--- include/utils.h
+++ include/utils.h
@@ -1,10 +1,20 @@
 #ifndef UTILS_H
 #define UTILS_H
 
-inline float absf (float x);
-inline double absd (double x);
+inline float absf (float x) {
+  if (x < 0) return (-1 * x);
+  return x;
+}
+inline double absd (double x){
+  if (x < 0) return (-1 * x);
+  return x;
+}
 
-inline float mn_squaref(float x);
-inline double mn_squared(double x);
+inline float mn_squaref(float x){
+  return x * x;
+}
+inline double mn_squared(double x){
+  return x * x;
+}
 
 #endif
